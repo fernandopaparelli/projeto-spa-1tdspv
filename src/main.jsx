3454861bba52514230beac1b695c80c5d7290466
fernandopaparelli <rm551022@fiap.com>
2023-08-29 22:59:18 -0300

    import

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,17 @@ import Produtos from './components/Produtos.jsx'
 import EditarProduto from './components/EditarProduto.jsx'
 import Error from './components/Error.jsx'
 
+
+const router = createBrowserRouter([
+  {path:"/", element:<App/>, children:[
+    {path: "/", element <Home/>},
+    {path: "/", element <Produto/>},
+    {path: "/", element <Error/>},
+    {path: "/", element <EditarProduto/>},
+  ]}
+])
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
